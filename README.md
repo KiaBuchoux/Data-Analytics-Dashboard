1824b5c31c47ca255a854d036fd892856adc10ef
KiaBuchoux <100135663+KiaBuchoux@users.noreply.github.com>
2022-02-28 15:04:24 +0100

    create repo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Data-Analytics-Dashboard
- Basic HTML CSS & JS
+
+- Fully responsive
+- Basic HTML CSS & JS
+
+![mobile-screenshot](Screenshot-mobile.png)
+![desktop-screenshot](Screenshot-desktop.png)
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-button").addEventListener("click", ()=>
+    document.querySelector("nav").style.transform = `translate(0)`
+)
+
+document.querySelector(".back-button").addEventListener("click", ()=>
+    document.querySelector("nav").style.transform = `translate(-100%)`
+)
